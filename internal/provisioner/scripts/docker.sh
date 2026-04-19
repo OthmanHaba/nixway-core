6d9613eb49c8b1b7c0999629eec547393f81f4ca
Othman Haba  (ง'̀-'́)ง <124089971+OthmanHaba@users.noreply.github.com>
2026-04-19 18:18:57 +0200

    fix: add --batch flag to gpg in Docker install script for non-interactive SSH

--- internal/provisioner/scripts/docker.sh
+++ internal/provisioner/scripts/docker.sh
@@ -8,7 +8,7 @@ apt-get update
 apt-get install -y ca-certificates curl gnupg
 
 install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/$(. /etc/os-release && echo "$ID")/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release && echo "$ID")/gpg | gpg --batch --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$(. /etc/os-release && echo "$ID") $(. /etc/os-release && echo "$VERSION_CODENAME") stable" > /etc/apt/sources.list.d/docker.list
